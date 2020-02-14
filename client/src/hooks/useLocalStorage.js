980daef2a8263b48635afb1286a17db4a65a54e0
Jake Johnson <jakejjohnson@gmail.com>
2020-02-14 11:39:50 -0600

    trying to fix Local Storage/Toggle

--- client/src/hooks/useLocalStorage.js
+++ client/src/hooks/useLocalStorage.js
@@ -1,16 +1,19 @@
-import { useState } from "react";
+import React, { useState } from 'react';
 
-export const useLocalStorage = (key, initialValue) => {
-  const [storedValue, setStoredValue] = useState(() => {
-    const item = window.localStorage.getItem(key);
-    return item ? JSON.parse(item) : initialValue;
-  });
+const useLocalStorage = (key, initialValue) => {
+    const [ storedValue, setStoredValue ] = useState(() =>{
+        const item = window.localStorage.getItem(key);
 
-  const setValue = value => {
-    setStoredValue(value);
-    window.localStorage.setItem(key, JSON.stringify(value));
-  };
+        return item ? JSON.parse(item) : initialValue;
+    });
 
+    const setValue = value => {
 
-  return [storedValue, setValue];
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value));
+    };
+
+    return [ storedValue, setStoredValue ]
 };
+
+export default useLocalStorage;
