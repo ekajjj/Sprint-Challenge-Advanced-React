91c1536c6906fe1a529c47ec6cdfe838e1b823d8
Jake Johnson <jakejjohnson@gmail.com>
2020-02-14 11:01:40 -0600

    setup, functional for now?

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import Data from "./components/Data";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      things: ""
+  
+    };
+  }
 
+  render() {
+  return (
+    <div className="App">     
+        <Data 
+            things={this.state.things}
+            />
+      </div>
+    );
+  }
+}
 export default App;
---
+++ client/src/components/Card.js
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "styled-components";
+
+//styled components
+
+const Box = styled.div `
+    border: pink 4px solid;
+    width: 60%;
+    border-radius: 20px;
+    margin: 0 auto;
+`
+
+const Player = styled.div `
+    border: 4px solid teal;
+    border-radius: 20px;
+    background: grey;
+    width: 20%;
+    height: 200px;
+    padding: 6%;
+    margin: 8%;
+`
+
+
+
+const Card = props => {
+
+    return (
+        <div>
+            <div>
+                <h1>Women's World Cup Players</h1>
+            </div>
+            <Box>
+                {props.things.map (item => {
+                    return (      
+                        <Player>           
+                        Player: {item.name} 
+                        <br></br>
+                        Country: {item.country} 
+                        <br></br>
+                        # of searches: {item.searches}
+                        </Player>    
+                    );
+                })}
+
+            </Box>
+        </div>
+    );
+};
+export default Card;
